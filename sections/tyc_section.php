499b769545d5da308ef5372bd8dba0d3e4a90ff4
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2019-01-11 21:44:47 -0400

    Agregamos pagina Terminos y Condiciones (tyc.php), imagenes y texto.

---
+++ sections/tyc_section.php
@@ -0,0 +1,52 @@
+<section>
+  <div class="container pt-5">
+    <div class="row">
+      <p>La aprobación de este paquete de vacaciones es con Orlando Yours LLC e incluye hospedaje en el destino seleccionado, (el Estado de la Florida), con una duración de 9 días y 8 noches en total, de acuerdo con el plan elegido. Este paquete de vacaciones es otorgado por la empresa ya mencionada, la cual está registrada en el estado de Florida y ubicada en 5091 NW 7th St, Miami Florida, 33126 Estados Unidos.</p>
+      <p>Las peticiones de reserva que no se reciban por lo menos (45) cuarenta y cinco días antes de la fecha estimada de viaje estarán sujetas a un cargo de $ 75.00 por familia. Titulares de paquetes de vacaciones deben notificar cualquier cambio a oficial@orlandoyours.com. El itinerario de Reservas sera enviado a su correo electrónico 7 días antes de su llegada a los Estados Unidos, esto dependerá de la fecha en que envíe su boleto aéreo y su planilla de reservas completamente llena.</p>
+      <p>Habrá cargos adicionales si los cambios se hacen 14 días antes de la fecha de llegada. Los cambios diferentes a la reserva existente tendrán un valor adicional de $ 25.00 USD por cada variante. Los cambios realizados dentro de (72) setenta y dos horas antes de su llegada tendrán un cargo de $99.00 USD.</p>
+      <p>Todas las reservaciones están sujetas a disponibilidad y capacidad de los hoteles ofertados en los destinos requeridos. La confirmación de la reserva será desde el departamento de reservas con detalles de fecha, lugar y hora de entrada en el destino. Para realizar las reservaciones, usted debe apartar sus fechas por medio de un correo electrónico a reservas@orlandoyours.com. certifique si sus días de viaje están disponibles y solicite asesoría para la compra de los boletos aéreos.</p>
+      <p>Estas vacaciones pueden excluir destinos de eventos especiales. Este paquete de vacaciones no es válido para los residentes donde la ley lo prohíba.</p>
+      <p>Esta oferta incluye hospedaje en Miami y/o Fort Lauderdale, y Orlando para un máximo de cuatro (4) personas, un auto compacto rentado incorporado para ustedes por el tiempo de su estadía.</p>
+      <p>Esta promoción no incluye el transporte a los destinos, impuestos de hotel, impuesto de vehículo, seguro del vehículo, impuesto de crucero, cargos por servicios, comidas, uso del teléfono internacional, boletos aéreos, boletos a parques, peajes y otros cargos. Prórroga de estancia para el viajero siempre será posible en función de la disponibilidad del hotel y sujeto a la variación de los precios de momento.</p>
+      <p>En esta promoción incluimos el servicio de traslado gratuito desde el aeropuerto internacional de Miami hasta el Rent a Cars asignado por la empresa (ida y vuelta). En caso de llegar por el aeropuerto internacional de Orlando u a otro aeropuerto del país, no podrá ser recogido por un operador de Orlando Yours, usted debe cubrir su traslado hasta el Rent a Cars contratado por la empresa.<br>
+      Por ser un programa vacacional adquirido a precio especial, no es reembolsable el dinero, puede transferir el programa vacacional de promoción a un familiar o amigo, sólo debe notificarlo al Departamento de Administración y enviar documentación del nuevo beneficiario del mismo, antes de cumplirse 6 meses de haber recibido el certificado.</p>
+      <p>Este paquete se puede transferir a cualquier persona que cumpla con los requisitos de calificación.</p>
+      <p>Esta promoción cumple con las leyes del Estado de Florida. El precio de promoción incluye en el Hotel de Orlando o Miami (dependerá de la asignación de su agente de reservas) un desayuno de cortesía, luego disfrutarán de 90 minutos en un Tour por las instalaciones en la que recibirán información acerca de cómo vacacionar en el futuro de una manera más fácil, económica y poder pertencer a nuestro selecto grupo de clientes VIP. En el momento de hacer su reserva, se le hará un cargo de $100.00 (cien) como garantía de que asistirá a nuestro tour de promoción y serán devueltos al finalizar este tour, por medio de una tarjeta VISA gift card.<br>
+      La fecha de este desayuno será asignada por el Departamento de Reservas del Hotel. En caso de no querer recibir este desayuno con tour, se le hará un cobro adicional al programa vacacional a precio especial de $550.00 (quinientos cincuenta)y no se le reintegrarán los $100.00 de garantía original. Orlando Yours LLC no tiene beligerancia en este asunto.</p>
+      <p>Cualquier reserva o un cambio de reserva está sujeta a disponibilidad. Las reservas deben hacerse a través del correo electrónico: reservas@orlandoyours.com</p>
+      <p>El propietario de este paquete debe tener mínimo 25 años de edad. Sólo califican familias, que estén casados o concubinos residenciados en el mismo domicilio. Con un ingreso de $50,000 (cincuenta mil) al año por grupo familiar. Ser portador de tarjeta de crédito Visa, MasterCard, American Express, Discover o Diners Club.</p>
+      <p>Orlando Yours LLC, no se responsabiliza de la pérdida del beneficio de descuento en cualquiera de las circunstancias de incumplimiento de los requisitos de la presentación, debe asistir con:</p>
+      <p>★ Dos formas de identificación con fotografía, tarjeta de crédito o débito.<br>
+      ★ El paquete de vacaciones tiene una vigencia de 10 (diez) meses A partir de la fecha de la compra. No hay restricción en la temporada, sin embargo, se basa en la disponibilidad de destinos.<br>
+      ★ El viaje debe completarse dentro de los 10 (diez) meses a partir de la promoción de compra (si no hay otro acuerdo por escrito) al vencimiento del paquete de vacaciones puede ampliarse a petición del cliente por 6 meses adicionales.<br>
+      ★ Promoción de %50 de descuento, realizará el primer pago de $799 (setecientos noventa y nueve) y el restante en cuotas fraccionadas por 3 (tres) meses.</p>
+      <p>★ En caso de poseer acuerdos de pago, el programa vacacional sólo puede pagarse libre de interés en un plazo de 3 meses, a partir de la compra, posterior a este tiempo, se le comenzarán a cobrar intereses. Para más información, puede escribirnos a el Departamento de Administración, a través de nuestro correo electrónico: administracion@orlandoyours.com</p>
+      <p>TÉRMINOS Y CONDICIONES DE ALQUILER DE VEHÍCULOS</p>
+      <p>★ El conductor debe tener al menos 21 años.<br>
+      ★ El conductor debe tener una licencia de conducir válida y legible de su país de residencia.<br>
+      ★ El conductor debe tener una tarjeta de crédito internacional<br>
+      ★ Términos y condiciones de los impuestos: no incluye seguro del vehículo, combustible, cargo por conductor adicional, parqueo, peajes y otros productos opcionales.</p>
+      <p>TÉRMINOS Y CONDICIONES DEL CRUCERO</p>
+      <p>★ El pasajero es responsable de pagar los impuestos. Las cabinas tienen un costo desde $ 199.00 por persona. Estos cargos no son reembolsables.<br>
+      ★ No incluye propinas.<br>
+      ★ En caso de que no desee tomar las dos noches del crucero, puede cambiarlas a noches en tierra, (Miami u Orlando) Con un cargo adicional de $160 como mínimo, ($80 por cada noche) este precio puede variar dependiendo de la temporada, puede recibir más información a través de nuestro correo electrónico: administracion@orlandoyours.com<br>
+      En caso de que desee tomar estas noches en otra ciudad, debe solicitar dicho cambio al departamento de Reservas, le darán precios actualizados por temporada.<br>
+      ★ Si usted es menor de 21 años de edad, no está permitido ingerir bebidas alcohólicas.</p>
+      <p>DOCUMENTACIÓN E IDENTIFICACIÓN REQUERIDA</p>
+      <p>Los requisitos de documentación de viaje varían según el destino y cambian a menudo. Es responsabilidad del viajero proporcionar documentos de viaje válidos requeridos para el aterrizaje y el reingreso al país de origen en cuestión. Se requiere la documentación apropiada de viajes en el momento del embarque y desembarque del crucero.<br>
+      Se les aconseja consultar con la autoridad gubernamental apropiada para determinar los documentos necesarios. Los huéspedes que viajan sin los documentos apropiados no se les permitirá subir a bordo.<br>
+      Los documentos deben ser presentados en el momento del envío. Los ciudadanos estadounidenses necesitan por lo menos uno de los siguientes:<br>
+      ★ Un pasaporte válido.<br>
+      ★ Un certificado de nacimiento original y una identificación con foto<br>
+      ★ Una copia certificada del acta de nacimiento emitida por el estado y una identificación con foto.<br>
+      ★ Carta de ciudadanía original.<br>
+      Embarque: Bahamas Celebration comienza el embarque a<br>
+      las 1:00 pm. El barco saldrá del puerto de West Palm Beach aproximadamente a las 5:30 de la tarde. Los huéspedes están obligados a estar a bordo del buque hasta a las 3 pm antes de la salida. El cliente debe completar el viaje y desembarcar en el puerto de embarque. Los pasajeros que parten del aeropuerto internacional Fort Lauderdale deben programar sus vuelos después de la 1:00 pm a disponer de tiempo suficiente.Orlando Yours no se hace responsable de las noches adicionales en caso que los clientes no tomen el crucero.</p>
+      <p>Jurisdicción<br>
+      Ud. acepta que todos los procedimientos conforme a Derecho o a los principios de equidad relativos a estas condiciones o a los Servicios de Orlando Yours, que no sean objeto de arbitraje deberán presentarse exclusivamente ante los tribunales estatales o federales del Estado de La Florida, Estados Unidos de América, a quienes corresponderá la jurisdicción exclusiva, y Ud. acepta someterse a la jurisdicción de dichos tribunales para la resolución judicial del correspondiente procedimiento.<br>
+      Ley Aplicable<br>
+      Estas condiciones se regirán e interpretarán de conformidad con las leyes del Estado de la Florida EE.UU., con exclusión de las normas sobre conflicto de leyes.<br>
+      IMPORTANTE: Al registrarse usted acepta ser mayor de 25 años y haber leído y aceptado los Términos y Condiciones de esta promoción.</p>
+    </div>
+  </div>
+</section>
---
+++ tyc.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<?php include 'sections/head.php'; ?>
+<body>
+	<!-- NAVBAR
+	================================================== -->
+	<?php include 'sections/navbar.php'; ?>
+
+	<!-- WELCOME
+	================================================== -->
+	<?php include 'sections/bienvenida_tyc.php'; ?>
+
+	<!-- NUESTRA FUNDACION
+	================================================== -->
+	<?php include 'sections/tyc_section.php'; ?>
+
+	<!-- FOOTER
+	================================================== -->
+	<?php include 'sections/footer.php'; ?>
+
+	<!-- JAVASCRIPT
+	================================================== -->
+	<?php include 'sections/javascript.php'; ?>
+
+	</body>
+</html>
